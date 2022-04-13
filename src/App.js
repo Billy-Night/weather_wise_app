249c245edacac616a8c3dcd5fbde55299275d2cc
Billy <billy.m.nightingale@gmail.com>
2022-04-13 15:25:02 +0200

    add initial funcitoning weather app with api and some inital sports information, also added an assets folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
+
 function App() {
+
+  let [weather, setWeather] = useState({});
+  let [apiLoaded, setApiLoaded] = useState(false);
+
+  let [city, setCity] = useState("");
+  let [showCity, setShowCity] = useState(false);
+
+
+  let apiCity = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=161b93914792ab2a2ebd537a08bb7915`;  
+
+
+  const handleChange = (event) => {
+    setCity(event.currentTarget.value); 
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setShowCity(true);
+  };
+
+  const weatherCall = () => {
+    fetch(apiCity)
+      .then((response) => response.json())
+      .then((data) => {
+        setWeather(data);
+        setApiLoaded(true);
+      });
+  };
+
+//   const cyclingRating = {
+//     realFeelTemperature: 4,
+//     WindSpeed: 3,
+//     Rain: 3,
+//   };
+//   console.log(cyclingRating);
+
+// const totalRating = (el) => {
+//     return el.realFeelTemperature + el.WindSpeed + el.Rain;
+// }
+// console.log(totalRating(cyclingRating))
+
+// const realFeelCycling = () => {
+//   let increaseinTemp = weather.main.feels_like - 26;
+//   return increaseinTemp;
+// }
+// console.log(realFeelCycling);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Please select your city</h1>
+      <form onSubmit={handleSubmit}>
+        <input value={city} onChange={handleChange} />
+        <button onClick={weatherCall}>Click</button>
+      </form>
+      {showCity ? (
+        <p>City you wrote is: {city}</p>
+      ) : (
+        <p>Write a city and click the button</p>
+      )}
+
+      {apiLoaded ? ( 
+      <>
+        <p>Location {weather.name} {weather.sys.country}</p>
+        <p>Tha temperature is {weather.main.temp}</p>
+        <p>The weather is {weather.weather[0].description}</p>
+        <p>The temperature feels like {weather.main.feels_like}</p>
+        <p>The visibility is {weather.visibility}</p>
+        <p>The wind speed is {weather.wind.speed} km/h</p>
+        <p>The humidity is {weather.main.humidity}%</p>
+      </>
+      ) : (
+        <h2>Loading weather</h2>
+      )}
+
+      <h1>Choose your sport</h1>
+      <button>Cycling</button>
+      <br></br>
+      <button>Surfing</button>
+      <br></br>
+      <button>Sailing</button>
+      <br></br>
+      <button>Hiking</button>
+      <br></br>
+      <button>Running</button>
+      <br></br>
+      <button>Snowboarding</button>
+      <br></br>
     </div>
   );
 }
