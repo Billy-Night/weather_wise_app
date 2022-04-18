fb1f27f2d94e241a6c96768dbd35880eac722ff1
Billy <billy.m.nightingale@gmail.com>
2022-04-18 17:34:02 +0200

    updated position of the rating number, all is ready for push update

--- src/components/CurrentRating.jsx
+++ src/components/CurrentRating.jsx
@@ -2,7 +2,7 @@ const CurrentRating = (props) => (
     <div ClassName="current-rating">
         {props.cyclingRating >= 0 ? (
       <>
-        <p>Todays cycling Rating is: 10/{props.cyclingRating}</p>
+        <p>Todays cycling Rating is: {props.cyclingRating}/10</p>
         <p>The parameters we checked for your day were:</p>
         <p>1. The real feel temperature is:{props.weather.current.feels_like}</p>
         <p>2. The wind speed is: {props.weather.current.wind_speed}km/h</p>
