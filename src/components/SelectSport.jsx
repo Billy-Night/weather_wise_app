8f95bb4a1b5bae2e42e9f68f45e31a2d59461003
Billy <billy.m.nightingale@gmail.com>
2022-05-02 18:39:19 +0200

    made a refactor to the sports logic, the sports are added to a new file called sportsSelector, the sportsLogic file contains all the logic for the rating system, MyProvider handles calling the required functions and passing the right parameters, everything is operational

--- src/components/SelectSport.jsx
+++ src/components/SelectSport.jsx
@@ -1,5 +1,7 @@
+
 import React, { useContext } from "react";
 import { MyContext } from "../context/MyProvider.js";
+import { sportsOptions } from "../utils/sportsSelector";
 
 const SelectSport = () => {
   const context = useContext(MyContext)
@@ -7,15 +9,22 @@ const SelectSport = () => {
 return (
     <div className="select-sport-header">
       <h1>Choose your sport</h1>
-      <div className="sport-btn-container">
-        <img src={context.cyclingImg} alt="cycling" onClick={context.handlClickCycle} />
-        <p>Cycling</p>
+      {sportsOptions.map((el, i) => (
+        <div className="sport-btn-container">
+          <img src={el.img} alt={el.name} onClick={() => context.handlClickSport(el.name)} />
+          <p>{el.name}</p>
+          </div>
+      ))}
       </div>
-      <div className="sport-btn-container">
-        <img src={context.runningImg} alt="running" onClick={context.handlClickRunning} />
-        <p>Running</p>
-      </div>
-    </div>
+    //   <div className="sport-btn-container">
+    //     <img src={context.cyclingImg} alt="cycling" onClick={context.handlClickCycle} />
+    //     <p>Cycling</p>
+    //   </div>
+    //   <div className="sport-btn-container">
+    //     <img src={context.runningImg} alt="running" onClick={context.handlClickRunning} />
+    //     <p>Running</p>
+    //   </div>
+    // </div>
 );
 };
 
---
+++ src/utils/sportsSelector.jsx
@@ -0,0 +1,14 @@
+import cyclingImg from "../assets/cyclingImg.png";
+import runningImg from "../assets/runningImg.png";
+
+export const sportsOptions = [
+    {
+      name: "cycling",
+      img: cyclingImg,
+    },
+    
+    {
+      name: "running",
+      img: runningImg,
+    },
+];
