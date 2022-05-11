1110b7f0412756a43e7e8b5bfd41b4c1e6b649da
Billy <billy.m.nightingale@gmail.com>
2022-05-11 02:31:29 +0200

    Major update to the current rating CSS with images and change button location

--- src/components/CurrentRating/CurrentRating.jsx
+++ src/components/CurrentRating/CurrentRating.jsx
@@ -3,6 +3,8 @@ import React, { useContext, useEffect, useState } from "react";
 import { MyContext } from "../../context/MyProvider.js";
 import ProgressBar from "../StatusBar/ProgressBar";
 import "../CurrentRating/CurrentRating.css";
+import airquality from "../../assets/airquality.png";
+import heat from "../../assets/heat.png";
 
 
 //changed to curly brackets, because there will be a return
@@ -43,27 +45,52 @@ const CurrentRating = () => {
                 progress={Number(context.cyclingRating)}
                 {...state} />
             </div>
-            <div className="rating-params">
+            <div className="rating-params-header">
               <h2>The weather factors are:</h2>
+            </div>
+            <div className="rating-params-temp">
               <p>
-                1. Real feel temperature is:{"   "}
+                1. Real feel temperature is:
                 {context.weather.current.feels_like.toFixed(0)}°C
               </p>
+            </div>
+            <div className="rating-params-temp-img">
+                <img src={heat} alt="wind" />
+            </div>
+            <div className="rating-params-wind">
               <p>
                 2. Wind speed is: {context.crtWindSpeed}km/h
               </p>
+            </div>
+            <div className="rating-params-wind-img">
+                <img src={context.windImg} alt="wind" />
+            </div>
+            <div className="rating-params-pop">
               <p>
                 3. Rain probability: {(context.weather.hourly[0].pop * 100).toFixed(0)}%
               </p>
+            </div>
+            <div className="rating-params-rain-img">
+                <img src={context.rainImg} alt="wind" />
+            </div>
+            <div className="rating-params-uvi">
               <p>4. UV index is: {context.weather.current.uvi}</p>
+            </div>
+            <div className="rating-params-uvi-img">
+              <img src={context.uvindexImg} alt="visibility" />
+            </div>
+            <div className="rating-params-air-pollution">
               <p>5. Air quality is: {context.airPollutionDes}</p>
+            </div>
+            <div className="rating-params-air-quality-img">
+              <img src={airquality} alt="visibility" />
             </div>
             <div className="rate-btn">
               <button onClick={context.handleNavCurrentWeather}>
                 Check Weather
               </button>
             </div>
-          </>
+            </>
         ) : (
           <div className="rating-not-loaded">
             <h3>The rating is not loaded</h3>
